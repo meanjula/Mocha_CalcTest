3865836058db4713b37dbb0da8c31b4e9233e978
meanjula <anjula.ghale@gmail.com>
2022-03-17 10:09:20 +0200

    testing typeof string number

--- test/calclibrary.test.js
+++ test/calclibrary.test.js
@@ -4,11 +4,18 @@ const expect = require("chai").expect;
 
 const { sum, subtract } = require("../calclibrary");
 
+//checking the typeof
 describe("Test if function are defined", function () {
-  it("sum is afunction", function () {
+  it("sum is a function", function () {
     expect(sum).to.be.a("function");
   });
-  it("sum is afunction", function () {
+  it("sum is a function", function () {
     expect(subtract).to.be.a("function");
+  });
+  it("2 is a string", function () {
+    expect("2").to.be.a("string");
+  });
+  it("2 is a number", function () {
+    expect(2).to.be.a("number");
   });
 });
